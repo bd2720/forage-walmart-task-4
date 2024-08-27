2fc2f013a8290a4fa0f926502cc4952e690f6ba1
bd2720 <brendandeneen9@gmail.com>
2024-08-27 14:18:48 -0400

    insert/delete test

--- script.py
+++ script.py
@@ -1,23 +1,25 @@
 import sqlite3
 # open connection to DB
-dbTestName = "shipment_database.TEST.db"
-dbName = "shipment_database.db"
+dbName = 'shipment_database.TEST.db'
 dbConnection = sqlite3.connect(dbName)
 # create cursor
 dbCursor = dbConnection.cursor()
-# query master table to see what tables exist
-tables = dbCursor.execute("SELECT name FROM sqlite_master")
-tables = tables.fetchall()
-print(tables)
-# display contents of each table
-for tableName in tables:
-  # skip table if created by sqlite
-  if tableName[0][0:7] == "sqlite_":
-    continue
-  res = dbCursor.execute("SELECT * from %s" % tableName)
-  print(tableName)
-  print(res.fetchall())
-
-# query from first db
+# insert into products table
+dbTable = 'product'
+prodID = 65
+prodName = 'bikes'
+dbCursor.execute('INSERT INTO %s VALUES(?, ?)'%dbTable, (prodID, prodName))
+# commit
+dbConnection.commit()
+# query
+queryResult = dbCursor.execute('SELECT * from ' + dbTable)
+print(queryResult.fetchall())
+# delete
+dbCursor.execute('DELETE FROM ' + dbTable)
+# commit
+dbConnection.commit()
+# query
+queryResult = dbCursor.execute('SELECT * from ' + dbTable)
+print(queryResult.fetchall())
 # close connection
 dbConnection.close()
